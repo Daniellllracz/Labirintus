36da9b445da3052b0ed8a0f59506ea303e1f5005
Daniellllracz <78079867+Daniellllracz@users.noreply.github.com>
2024-09-03 11:21:16 +0200

    Add files via upload

---
+++ LABIRINTUS/LabSim.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labirintus
+{
+    internal class LabSim
+    {
+        private List<string> adatsorok;
+        private char[,] lab;
+
+        public void AdaatsorokBeolvasasa(string forras)
+        {
+            using StreamReader sr = new(forras);
+            while (!sr.EndOfStream) adatsorok.Add(sr.ReadLine());
+        }
+        public LabSim(string fileName)
+        {
+            using StreamReader sr = new($@"..\..\..\src\{fileName}");
+
+        }
+    }
+}
